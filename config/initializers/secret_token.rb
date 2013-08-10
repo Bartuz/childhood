2c4a36eb9d066b9cab8248ad2c1ca188edbf49db
Maxime Rousseaux-Bridle <max@12gsolutions.com>
2013-08-10 10:30:47 -0400

    Added stock rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LearningLoop::Application.config.secret_key_base = '5b848a71cf9c662ea5ec12b15400c90a80817ed220a31881f8faab8b2efce1e1f689487ab20791e993765326c339e63a9d57b8ac195c5b78842a5a6a3a77e48f'
