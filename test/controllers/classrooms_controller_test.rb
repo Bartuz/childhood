64bdd98133ce4b15f8a759509e4dc572095db70d
Bartuz <iamnotawowplayer@gmail.com>
2013-08-10 15:41:50 -0400

    Classroom controller = classrooms controller

--- test/controllers/classrooms_controller_test.rb
+++ test/controllers/classrooms_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class ClassroomControllerTest < ActionController::TestCase
+class ClassroomsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
