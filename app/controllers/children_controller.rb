d267559eb31cce4a93b22af05419ea222ff8fb91
Cory Taylor <ctaylor@kobo.com>
2013-08-10 16:05:05 -0400

    renamed educator to educators; added link to activity; added view for activity

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -17,4 +17,15 @@ class ChildrenController < ApplicationController
       format.json { render json: @project_hash }
     end
   end
+
+  def activity
+
+    @child = Child.find(params[:id])
+    @events = Event.where(child_id:params[:id])
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @project_hash }
+    end
+  end
 end
--- test/controllers/educators_controller_test.rb
+++ test/controllers/educators_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class EducatorControllerTest < ActionController::TestCase
+class EducatorsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
