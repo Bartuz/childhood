dc5ae3ae9b939b3a76290ab3074a6bbca772617c
Bitmaker Labs <bitmakerlabs@Bitmakers-MacBook-Air.local>
2013-08-10 14:19:39 -0400

    Self joins in category works

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130810180309) do
+ActiveRecord::Schema.define(version: 20130810181701) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -19,9 +19,11 @@ ActiveRecord::Schema.define(version: 20130810180309) do
     t.integer  "category_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "parent_category_id"
   end
 
   add_index "categories", ["category_id"], name: "index_categories_on_category_id"
+  add_index "categories", ["parent_category_id"], name: "index_categories_on_parent_category_id"
 
   create_table "children", force: true do |t|
     t.string   "first_name", null: false
